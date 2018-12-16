e21fb52fdc37876e70267e17679805138a2f9773
xiongzhiwen <353210321@qq.com>
2018-12-16 20:16:12 +0800

    checkout -- file

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		int i =1;
 		int b =0;
-		System.out.print(i++);
+		System.out.print(i++);System.out.print(i+b);
 		System.out.println("hhhhh");
 	}
 }
